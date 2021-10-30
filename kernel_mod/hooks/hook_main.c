79f2c0ea0e27619baf18444f88e332e8a91da723
RicheyJang <773602577@qq.com>
2021-10-30 12:34:58 +0800

    fix hook action

--- kernel_mod/hooks/hook_main.c
+++ kernel_mod/hooks/hook_main.c
@@ -4,18 +4,16 @@
 const unsigned int DEFAULT_ACTION = NF_ACCEPT;
 
 unsigned int hook_main(void *priv,struct sk_buff *skb,const struct nf_hook_state *state) {
-    unsigned int action;
+    unsigned int action = DEFAULT_ACTION;
     // 匹配规则
     int isMatch = 0;
     struct IPRule rule = matchIPRules(skb, &isMatch);
     if(isMatch) { // 匹配到了一条规则
-        printk("[fw netfilter]patch rule %s.\n", rule.name);
+        printk("[fw netfilter] patch rule %s.\n", rule.name);
         action = (rule.action==NF_ACCEPT) ? NF_ACCEPT : NF_DROP;
         if(rule.log) { // 记录日志
             addLogBySKB(action, skb);
         }
-        return action;
     }
-    // 默认动作
-    return DEFAULT_ACTION;
+    return action;
 }
